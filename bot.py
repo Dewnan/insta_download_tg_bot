decb304f2be9ff1a4606da6651c290abd9f2b001
Dewnan <70105964+Dewnan@users.noreply.github.com>
2025-04-27 14:22:33 +0000

    switched to instaloader and a way to handle images and videos (idk what happen to Carousels)

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters, CommandHandler
-import yt_dlp
+import instaloader
 
 os.makedirs("download", exist_ok=True)
 
@@ -14,13 +14,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Send me an Instagram link and I will send you the media. develop by Dewnan.")
+
 async def handle_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = update.message.text
+
     if "instagram.com" in url:
         await update.message.reply_text("Downloading...")
+
         try:
             await download_content(url)
             await update.message.reply_text("Download Complete.")
+
         except Exception as e:
             await update.message.reply_text("Error downloading: Internal server error.")
             print(f"Error: {e}")
@@ -28,18 +32,38 @@ async def handle_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("This is not a valid Instagram link.")
 
 async def download_content(url):
-        ydl_opts = {
-            'outtmpl': 'download/%(title).30s.%(ext)s',
-            'quiet': True,
-            'format': 'bestvideo+bestaudio/best',
-            'skip_download': False,
-            'noplaylist': False,
-        }
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            print("start downloadng.")
-            ydl.extract_info(url, download=True)
-            
+    image_loader = instaloader.Instaloader(
+        save_metadata=False,
+        download_video_thumbnails=False,
+        download_pictures=True,
+        download_videos=False,
+        quiet=True,
+    )
+
+    video_loader = instaloader.Instaloader(
+        save_metadata=False,
+        download_video_thumbnails=False,
+        download_pictures=False,
+        download_videos=True,
+        quiet=True,
+    )
+    try:
+        post = instaloader.Post.from_shortcode(image_loader.context, url.split("/")[4])
+        if post.is_video:
+            loader = video_loader
+            print("Using Video Loader.")
+        else:
+            loader = image_loader
+            print("Switch to Image Loader.")
+
+        print(f"Downloaded...")
+        loader.download_post(post, target=f"download'/'{post.owner_username}")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
